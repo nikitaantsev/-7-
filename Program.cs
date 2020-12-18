dee8bd4336e6489abc771b2c44e92115fa932a42
nikitaantsev <71016018+nikitaantsev@users.noreply.github.com>
2020-12-18 19:27:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace лаб_7
+{
+    class Kvadrat
+    {
+        protected int storona;
+
+        public Kvadrat()
+        {
+            storona = 0;
+        }
+
+        public Kvadrat(int st)
+        {
+            storona = st;
+        }
+
+        public int Storona
+        {
+            get { return storona; }
+            set { storona = value; }
+        }
+
+        public virtual string Info()
+        {
+            return string.Format("Kvadrat");
+        }
+
+        public virtual int Area()
+        {
+            return storona * storona;
+        }
+
+        public virtual int Perimeter()
+        {
+            return 4 * storona;
+        }
+
+        public virtual double Diagonal()
+        {
+            return Math.Sqrt(2 * storona * storona);
+        }
+    };
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int storona;
+            Console.WriteLine("Введите длину стороны :");
+            storona = Convert.ToInt32(Console.ReadLine());
+            Kvadrat kv1 = new Kvadrat(storona);
+
+            Console.WriteLine(kv1.Info());
+            Console.WriteLine("Area {0}", kv1.Area());
+            Console.WriteLine("Volume {0}", kv1.Perimeter());
+            Console.WriteLine("Diagonal {0}", kv1.Diagonal());
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
